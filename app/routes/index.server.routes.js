d97b12265adf1225a8d0a0740fed43f516aba199
Alex Lyons <lyonsalexm@gmail.com>
2016-10-03 23:41:44 -0700

    updates

--- app/routes/index.server.routes.js
+++ app/routes/index.server.routes.js
@@ -1,16 +1,4 @@
 module.exports = function(app) {
-
-
     var index = require('../controllers/index.server.controller');
-	var ProjectDataModel = require('../models/projectdata');
-
     app.get('/', index.render);
-    app.get('/api/data', function(req, res) {
-        ProjectDataModel.find(function(err, data) {
-            if (err)
-                res.send(err);
-            res.json(data); // return all data in JSON format
-        });
-    });
-
 };

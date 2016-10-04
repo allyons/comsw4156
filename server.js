a6f3a564ad7ff73e0f232932189af96abfb167a7
Alex Lyons <lyonsalexm@gmail.com>
2016-10-04 00:35:21 -0700

    changes

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var port = 1337;
 var express = require('./config/express');
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://localhost', ['restaurants']);
 var app = express();
 app.listen(port);
 module.exports = app;
 console.log('Server running at http://localhost:' + port);
+
+app.get('/restaurants', function (req, res) {
+  db.restaurants.find(function (err, docs) {
+    res.json(docs);
+  });
+});
